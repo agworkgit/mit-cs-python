a1c46774c1a4ce243da49479ee4d1b22201ad3a6
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-12-25 01:32:43 +0000

    chapter2 exercises

---
+++ chapter2/index.py
@@ -0,0 +1,47 @@
+print('Yankees rule!')
+print('But not in Boston!')
+
+# The print function takes a variable number of arguments separated by commas
+
+print('Yankees rule,','but not in Boston!')
+
+# Scalar objects
+# 1. int (-/+ nums)
+# 2. float (decimal point nums)
+# 3. bool (true/false)
+# 4. None
+
+# Objects and Operators
+# - Combined to form Expressions
+
+print(3 + 2)
+print(3.0 + 2.0)
+print(3 != 2)
+
+# Finding object type
+
+print(type(3))
+print(type(3.0))
+print(type('3'))
+
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+
+# The primitive operators on type bool are and, or, and not
+
+# Binding to variables
+
+pi = 3
+radius = 11
+area = pi * (radius**2)
+radius = 14
+
+print(pi, radius, area, radius)
+# 3, 14, 363, 14
+
+# In Python, a variable is just a name, nothing more. Remember this—it is im-
+# portant. An assignment statement associates the name to the left of the = symbol
+# with the object denoted by the expression to the right of the =
+# . Remember this
+# too. An object can have one, more than one, or no name associated with it.
